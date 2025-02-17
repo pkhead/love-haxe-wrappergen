30b0f8455b74e44d02ae7c4a6a980c5131b50ffa
pkhead <chromosoze@gmail.com>
2025-02-17 00:21:18 -0600

    update TextDrawing.hx

--- tests/src/TextDrawing.hx
+++ tests/src/TextDrawing.hx
@@ -1,6 +1,8 @@
+import love.graphics.GraphicsModule as LoveGraphics;
+
 class TextDrawing extends love.Application {
     override function draw() {
-        love.Graphics.print("Hello, world!", 400, 300);
+        LoveGraphics.print("Hello, world!", 400, 300);
     }
 
     public static function main() {
